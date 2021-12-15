81eea82e6683371cf38e485a88481d7f63c111e3
abhisheksista <89856036+abhisheksista@users.noreply.github.com>
2021-12-15 14:28:44 +0530

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -1,7 +1,12 @@
-x=int(input("Enter starting value of range: "))
-y=int(input("Enter ending value of range: "))
-i=x
-while i<=y:
-    if i>0:
-        print(i)
-    i=i+1
+i=int(input("Enter length of a list: "))
+print("Enter a range of random numbers: ")
+list1=[]
+list2=[]
+while i>0:
+    x=int(input())
+    list1.append((x))
+    i=i-1
+for number in list1:
+    if number>0:
+        list2.append((number))
+print("The positive numbers in given list are:",list2)
