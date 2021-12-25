b4aa2cee9316678c5174430d98fe76a80ae86dda
abhisheksista <89856036+abhisheksista@users.noreply.github.com>
2021-12-25 08:58:21 +0530

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -1,3 +1,4 @@
+#positive numbers in a range
 i=int(input("Enter length of a list: "))
 print("Enter a range of random numbers: ")
 list1=[]
@@ -10,3 +11,17 @@ for number in list1:
     if number>0:
         list2.append((number))
 print("The positive numbers in given list are:",list2)
+
+#fibonacci series
+n=int(input("enter number of values in fibonacci series: "))
+a=0
+b=1
+n=n-2
+print(a)
+print(b)
+while n>0:
+    c=a+b
+    print(c)
+    a=b
+    b=c
+    n=n-1
